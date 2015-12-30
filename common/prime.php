20ceb74374cc5777702ac347047b9bef479157f7
Steven Attevelt <sattevelt@gmail.com>
2015-12-30 14:28:09 +0100

    -

--- common/prime.php
+++ common/prime.php
@@ -2,12 +2,26 @@
 
 class Prime
 {
+    public static $primes = array(2 => 2);
 
     public static function isPrime($number)
     {
+        if (array_key_exists($number, self::$primes)) {
+            return true;
+        } else if ($number < reset(self::$primes)) {
+            return false;
+        }
+
+        $max = (int)floor($number / 2);
         
-
-
+        for ($i = 2; $i <= $max; $i++) {
+            if ($number % $i == 0) {
+                return false;
+            }
+        }
+        
+        self::$primes[$number] = $number;
+        return true;
     }
 
 
--- problem3.php
+++ problem3.php
@@ -3,6 +3,17 @@
 require_once './common/prime.php';
 
 
-$number = 13195;//600851475143;
+$number = 600851475143;
+$max = (int)ceil($number / 2);
+$factor = 2;
 
 
+while ($number > 1) {
+    if ($number % $factor == 0) {
+        $number = (int) $number / $factor;
+    } else {
+        $factor++;
+    }
+}
+
+var_dump($factor);
